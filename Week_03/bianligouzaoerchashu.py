e0080347dfed4baf442c61f3a34f19408b0e3997
Geekuniversity <geekuni@geekbang.com>
2020-07-26 23:29:30 +0800

    week03

---
+++ Week_03/bianligouzaoerchashu.py
@@ -0,0 +1,15 @@
+#从前序与中序遍历序列构造二叉树
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        if not preorder or not inorder:
+            return None
+        root = TreeNode(preorder[0])
+        index = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:index+1], inorder[:index])
+        root.right = self.buildTree(preorder[index+1:], inorder[index+1:])
+        return root
